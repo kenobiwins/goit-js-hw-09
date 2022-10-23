14a19a6106ef80ce2bb70181152655ef5141fe08
Max Sidorenko <fivemoshr@gmail.com>
2022-10-23 13:01:21 +0300

    add-tas1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+// const getEl = selector => document.querySelector(selector);
 
+// getEl('[data-start]').addEventListener('click', startRandomBackgroundSwitch);
+// getEl('[data-stop]').addEventListener('click', () => console.log('stop'));
+
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.start.addEventListener('click', startRandomBackgroundSwitch);
+refs.stop.addEventListener('click', stopRandomBackgroundSwitch);
+
+let intervalId = null;
+
+function startRandomBackgroundSwitch({ currentTarget }) {
+  const changeBackgroundColor = () =>
+    (document.body.style.backgroundColor = getRandomHexColor());
+  changeBackgroundColor();
+
+  intervalId = setInterval(() => {
+    changeBackgroundColor();
+  }, 1000);
+
+  currentTarget.setAttribute('disabled', '');
+}
+
+function stopRandomBackgroundSwitch(e) {
+  clearInterval(intervalId);
+  refs.start.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
